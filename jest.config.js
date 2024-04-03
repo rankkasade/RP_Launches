b42430110f9e59177cc4e707b3fb3c067024e249
Hanna Rykouskaya <hanna_rykouskaya@epam.com>
2024-04-04 01:22:32 +0200

    added maxConcurrency to Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     preset: 'jest-playwright-preset',
+    maxConcurrency: 3,
     testMatch: ['<rootDir>/src/tests/**/*.j-test.js'],
     reporters: ["jest-allure"],
     moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
